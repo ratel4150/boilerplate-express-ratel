d4e10fa3ee0caf4644bb71007877b2a80e2113b2
Ratel Ratel <97288130+ratel4150@users.noreply.github.com>
2022-01-31 21:34:34 +0000

    Ejercicio numero 6

--- myApp.js
+++ myApp.js
@@ -2,10 +2,36 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 
-app.get("/", function(req, res) {
+/*app.get("/", function(req, res) {
   res.sendFile( __dirname + "/views/index.html");
 });
 
+app.use("/", function(req, res) {
+  res.sendFile( __dirname + "/style.css");
+});*/
+
+/*app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});*/
+
+/******5*****//*app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json
+  });
+});
+*/
+app.use('/json', (req, res) => {
+  let message = "Hello json"
+  
+  if (process.env.MESSAGE_STYLE==='uppercase') {
+    return res.json({"message": message.toUpperCase()});
+  }
+  else {
+    return res.json({"message": message})
+  };
+});
 
 
 
